71f9bdafce58546ab19cbebb27ffcf1bd7a8fe9f
ZURSONA777 <sayvarelo3@gmail.com>
2024-05-12 17:10:30 +0400

    commit

--- class work (1)/classworkanother.py
+++ class work (1)/classworkanother.py
@@ -4,3 +4,4 @@ user_input = int(input("plase enter whole number: ") )
 
 for i in range(0, user_input + 1 ):
     print(i)
+
---
+++ day14/day14.py
@@ -0,0 +1,73 @@
+#დავალება 1
+
+#user = int(input("please enter number:"))
+
+#for i in range(0,  user + 1 ):
+    #Print(i)
+
+
+
+
+#cars = ["BMW", "PORSCHE", "AUDI"]
+#for i in cars :
+    #print(i)
+
+
+
+#user = int(input("please enter number:"))
+
+#for i in range(0,  user - 1 ):
+    #print(i)
+
+
+
+#user_input = int(input("plase enter whole number: ") )
+
+#for i in range(0, user_input + 1 ):
+    #print(i)
+
+#fruits = ["apple", "banana", "lime"]
+#for i in fruits:
+  #print(i)
+
+
+#დავალება 2
+
+#i = 1
+#while i < 6:
+  #print(i)
+  #i += 1
+
+
+#i = 2
+#while i < 10:
+ # print(i)
+  #i += 1
+#
+# i = 5
+#while i <20:
+  #print(i)
+ # i += 1 
+
+
+#i = 7
+#while i < 40:
+#  print(i)
+#  i += 1
+
+
+#i = 10
+#while i < 100:
+ # print(i)
+  #i += 1
+
+
+#დავალება 3
+
+
+sum = 0
+for i in range(51):
+    if i % 5 == 0:
+        sum += i
+print(sum)
+
