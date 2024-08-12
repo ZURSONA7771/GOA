7697d64a46a8bb894a29dedf75cd9254ad448b1f
ZURSONA777 <sayvarelo3@gmail.com>
2024-08-12 22:22:25 +0400

    commit

---
+++ day07/homework/day07.py
@@ -0,0 +1,27 @@
+#დავალება 1
+# num = int(input('please enter number in c: '))
+# print((num * 1.8) + 32, 'f' )
+
+#დავალება 2
+
+# user = int(input('please enter your age: '))
+# if user < 12 or  user > 20: 
+#     print('you are adult')
+# else: 
+#     print('you are young')
+
+
+#დავალება 3
+
+# side1 = float(input('please enter first side length: '))
+# side2 = float(input('please enter second side length: '))
+
+# print('area of this rectangle is: ',side1 * side2)
+
+#დავალება 4
+
+# name1 = int(input('please enter number: '))
+# if name1  < 100 and  name1 / 9:
+#     print('bad job')
+# else:
+#     print('good job')
---
+++ day09/homework/day09.py
@@ -0,0 +1,9 @@
+# num3 = 1000
+# num2 = 10000
+# num1 = int(input('please enter salary: '))
+# if num1 > num2:
+#     print('are you studing in GOA?')
+# elif num1 < num2 and num1 > num3:
+#     print('you are chad')
+# else:
+#     print('you must join GOA')
