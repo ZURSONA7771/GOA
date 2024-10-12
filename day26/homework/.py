1fe47b8586e714a3e42f509b2bdf5a7f0ec38dc8
ZURSONA777 <sayvarelo3@gmail.com>
2024-10-12 17:01:57 +0400

    second commit

--- day26/homework/.py
+++ day26/homework/.py
@@ -1,1 +1,32 @@
-#
+user = str(input('please eneter nummber :'))
+user2 = str(input('please enter second number :'))
+
+# print(user + user2)
+for i in range (0,100,2) :
+    print(i)
+
+
+
+# მომხმარებელს გამოაცნობინე შენი რიცხვი, ანუ შექმენი რაიმე ცვლადი და შეინახე შენი რიცხვი, და შემდეგ მომხმარებელს უთხარი რომ guess number 1 to 50 ანუ გამოიცნოს რიცხვი 1 დან 50 მდე და სიტყვაზე შენი რიცხვი თუ იქნება 10 და მომხმარებელმა თუ ჩაწერა 10 დაუპრინტე რომ სწორია და სხვა შემთხვევაში else: დაუპრინტე რომ არასწორია
+
+# user = 10
+# user1 = int(input('please guess my number :'))
+# if user1 == 10 :    
+#     print('correct')
+# else :
+#     print('not correct')
+
+
+# user = 7
+# while user > 20 :
+
+# user = str(input('please eneter nummber :'))
+# user2 = str(input('please enter second number :'))
+
+# print(user + user2)
+# for i in range (0,100,2) :
+#     print(i)
+
+user = str(input('please enter number :'))
+
+for i in range
