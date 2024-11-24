409c4980e7825c583a139e3cbc22a34024bc9b9e
ZURSONA777 <sayvarelo3@gmail.com>
2024-11-24 17:21:27 +0400

    commit

---
+++ day44/classwork/.py
@@ -0,0 +1,19 @@
+
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def name(bot):
+    next = bot[0].lower()
+    if next in alphabet:
+        finish = alphabet.index(next) + 1
+        return f"first letter of the game'{next}' is {finish}  in alphabet order."
+    else:
+        return 'the frist letter of  the name is not letter of the alphabet..'
+    
+bot = input('enter your name:')
+print(name(bot))
+
+
+
+
