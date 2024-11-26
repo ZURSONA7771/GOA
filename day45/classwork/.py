b401faa80cf6b15e06b9d4b685bd37c93dca5471
ZURSONA777 <sayvarelo3@gmail.com>
2024-11-26 14:39:28 +0400

    commit

--- day45/classwork/.py
+++ day45/classwork/.py
@@ -1,1 +1,21 @@
-#codedd wars
+#codewars
+
+
+#1
+#def solution(text, ending):
+#   return text.endswith(ending)
+
+#2
+#select *
+#from users
+#where age > 17;
+
+#3
+#Select name,country from travelers where country not in ('Canada','Mexico','USA')
+
+#4
+
+#describe :query do
+ # describe "should contain keywords" do
+  #  it "should contain SELECT" do
+   #   expect($sql.upcase).to include("SELECT")
