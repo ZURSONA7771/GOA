358dc74aab3ba3bd3820e3cbb960b0a872b14fbb
ZURSONA777 <sayvarelo3@gmail.com>
2024-12-07 14:13:46 +0400

    commit

---
+++ day48/homework/.py
@@ -0,0 +1,27 @@
+#code wars
+
+#1
+#def fixed_tests():
+ #   @test.it("one")
+  #  def _():
+   #     test.assert_equals(human_years_cat_years_dog_years(1), [1,15,15])
+    #@test.it("two")
+    #def _():
+      #  test.assert_equals(human_years_cat_years_dog_years(2), [2,24,24])
+    #@test.it("ten")
+
+
+#2
+
+#is_prime(1)  /* false */
+#is_prime(2)  /* true  */
+#is_prime(-1) /* false */
+
+
+#3
+#
+#def basic_test_cases():
+ #       test.assert_equals(other_angle(30, 60), 90)
+  #      test.assert_equals(other_angle(60, 60), 60)
+   #     test.assert_equals(other_angle(43, 78), 59)
+    #    test.assert_equals(other_angle(10, 20), 150)
